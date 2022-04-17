b7cac72f651eba9c91f3991e2c64a29f6977408f
bita.karvizi <bita.karvizi1379@gmail.com>
2022-04-17 14:05:45 +0430

    Implement base of API(#4)
    * Add resource entities and files

---
+++ app/src/main/java/com/example/apisample/core/entity/Resource.kt
@@ -0,0 +1,46 @@
+package com.example.apisample.core.entity
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val status: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+
+    fun handle(tag: String, context: Context, errMsg: String): T? {
+        return handle(tag, context, errMsg, {}, {})
+    }
+
+    fun handle(
+        tag: String,
+        context: Context,
+        errMsg: String,
+        startLoad: () -> Unit,
+        endLoad: () -> Unit
+    ): T? {
+        return when (this) {
+            is Error -> {
+                endLoad()
+                Log.e(tag, "Failed to load resources from server.")
+                Toast.makeText(context, errMsg, Toast.LENGTH_SHORT).show()
+                null
+            }
+            is Success -> {
+                endLoad()
+                Log.i("bita "+tag, data.toString())
+                Log.e(tag, "Resources loaded successfully.")
+                this.data
+            }
+            is Loading -> {
+                startLoad()
+                Log.e(tag, "Loading resources ...")
+                this.data
+            }
+        }
+    }
+}
