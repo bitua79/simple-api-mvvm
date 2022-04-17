9a5dea2ca44511b9c851879663ddb4b1437ba143
bita.karvizi <bita.karvizi1379@gmail.com>
2022-04-17 11:23:58 +0430

    Implement MainActivity(#2)
    * Add activity_main.xml and implement fragment container view

--- app/src/main/java/com/example/apisample/MainActivity.kt
+++ app/src/main/java/com/example/apisample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.apisample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.apisample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initNavController()
+    }
+
+    private fun initNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
