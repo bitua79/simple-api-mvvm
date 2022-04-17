f57decaee1c14a8c6ac77a9fe47e51e39b322a51
bita.karvizi <bita.karvizi1379@gmail.com>
2022-04-17 13:36:47 +0430

    Implement xml layouts(#3)
    * Add fragment_anime_list.xml
    * Implement item_anime.xml

--- app/src/main/java/com/example/apisample/AnimeListFragment.kt
+++ app/src/main/java/com/example/apisample/AnimeListFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.apisample.databinding.FragmentBlankBinding
+import com.example.apisample.databinding.FragmentAnimeListBinding
 
-class BlankFragment: Fragment() {
+class AnimeListFragment: Fragment() {
 
-    private lateinit var binding: FragmentBlankBinding
+    private lateinit var binding: FragmentAnimeListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentBlankBinding.inflate(inflater, container, false)
+        binding = FragmentAnimeListBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
